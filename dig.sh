c33173d1825fd29b64be292fd6d4cfc383093e6b
TripleYei <58819156+TripleYei@users.noreply.github.com>
2022-11-07 23:23:26 +0100

    Add files via upload

---
+++ dig.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+
+cat << EOF
+
+ ######  ###  #####  
+ #     #  #  #     # 
+ #     #  #  #       
+ #     #  #  #  #### 
+ #     #  #  #     # 
+ #     #  #  #     # 
+ ######  ###  #####  
+                      
+                         
+Triple yei
+
+
+EOF
+
+
+echo "Introduce dominio a analizar"
+read nombre
+dig $nombre
